e08747717f78d1c6eac193fbba6fd9e164a859e9
Brooke <brookechook659@gmail.com>
2024-09-03 17:30:51 +1000

    origin/main: added skeleton code for future svgs and functionality. This is not linked to an fr

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function init() {
+    console.log("js works")
+    // todo: need to get dataset
+
+    const svg1 = d3.select("#child-vac-rates-svg");
+    const svg2 = d3.select("#alc-cons-svg");
+
+}
+
+
+window.onload = init;
