4755cb0551f38c01c7aa2813178682d5959ee80f
Jacob Coulston <165617458+Jcou003@users.noreply.github.com>
2024-09-29 15:51:56 +1000

    DV-1.0 Base structure completion
    Update csv access pathing before merge to main do to incompatibilities with mercury web server.

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,4 @@
 function init() {
-    let sampleData = [{ // todo: need to get dataset, this'll do for now (files will be year-relevant)
-        country: "Australia",
-        rate: 10
-    }, {
-        country: "Belgium",
-        rate: 100,
-    }, {
-        country: "Turkey",
-        rate: 40,
-    }, {
-        country: "Japan",
-        rate: 90,
-    }, {
-        country: "Estonia",
-        rate: 70,
-    }, {
-        country: "India",
-        rate: 90,
-    }]
     const s_width = 700;
     const s_height = 500;
     const svgPadding = 40;
@@ -120,7 +101,92 @@ function init() {
     }
 
     function generateSvgDataChld(data) {
-        //for svg1
+        //updated x and y for dynamic scaling:
+        const scaleX = d3.scaleLinear() //scale linear as non-bar chart graph
+            .domain([1, data.length])
+            .range([svgPadding, s_width])
+        const scaleY = d3.scaleLinear()
+            .domain([0, 100])
+            .range([s_height, 0]);
+
+        //Add ticks and adjust sizing
+        const xAxis = d3.axisBottom(scaleX).tickSize(15);
+        const yAxis = d3.axisLeft(scaleY).ticks(10).tickSize(15);
+
+        //Horizontal ine for when a bar is hovered over
+        const hoverLine = svg1.append("line")
+            .attr("class", "highlight")
+            .attr("x1", 0)
+            .attr("x2", s_width)
+            .attr("y1", s_height)
+            .attr("y2", s_height)
+            .style("opacity", 0);
+
+
+        //vacc-rate-svg dtp dots
+        let circ1 = svg1.selectAll("rect").data(data).enter()
+            .append("circle")
+            .attr("visibility", function(d){if (d.dtp) return "visible"; else return "hidden";}) //hide dot if value doesn't exist
+            .attr("class", "circ-c")
+            .attr("stroke", "black")
+            .attr("stroke-width", "2px")
+            .attr("fill", "#9e9ac8")
+            .attr("cx", function(d, i){return (scaleX(i+1));})
+            .attr("cy", function(d){return scaleY(d.dtp);})
+            .attr("r", 3)
+            //mouseover effects for line
+            .on("mouseover", function(event, d) {
+                const cirY = scaleY(event.dtp);
+                //specify dimensions of horizontal line and make visible
+                hoverLine
+                    .attr("x1", svgPadding) // Start position moved left
+                    .attr("x2", s_width + svgPadding)
+                    .attr("y1", cirY)
+                    .attr("y2", cirY)
+                    .attr("id", "line-bar")
+                    .style("opacity", 0.5);
+            })
+            .on("mouseout", function (event, d) {
+                hoverLine.style("opacity", 0);
+            })
+        
+            //vacc-rate-measles-dots
+        let circ2 = svg1.selectAll("rect").data(data).enter()
+            .append("circle")
+            .attr("visibility", function(d){if (d.Measles) return "visible"; else return "hidden";}) //hide dot if value doesn't exist
+            .attr("class", "circ-c")
+            .attr("stroke", "black")
+            .attr("stroke-width", "2px")
+            .attr("fill", "#e61017")
+            .attr("cx", function(d, i){return (scaleX(i+1));})
+            .attr("cy", function(d){return scaleY(d.Measles);})
+            .attr("r", 3)
+            //mouseover effects for line
+            .on("mouseover", function(event, d) {
+                const cirY = scaleY(event.Measles);
+                //specify dimensions of horizontal line and make visible
+                hoverLine
+                    .attr("x1", svgPadding) // Start position moved left
+                    .attr("x2", s_width + svgPadding)
+                    .attr("y1", cirY)
+                    .attr("y2", cirY)
+                    .attr("id", "line-bar")
+                    .style("opacity", 0.5);
+            })
+            .on("mouseout", function (event, d) {
+                hoverLine.style("opacity", 0);
+            })
+
+        //axis lines
+        svg1.append("g")
+            .call(xAxis)
+            .attr("class", "axis x-axis")
+            .attr("transform", `translate(0, ${s_height})`).style("font-size", "14px");
+
+        svg1.append("g")
+            .call(yAxis)
+            .attr("class", "axis y-axis")
+            .attr("transform", `translate(${svgPadding},  0)`).style("font-size", "14px");
     }
 
 
@@ -133,11 +199,13 @@ function init() {
     });
 
     //default value used here
-    d3.csv(`../DV_CSVs/Alcohol Consumption/AC_2023.csv`).then(data => {
+    d3.csv(`../assign3/DV_CSVs/Alcohol Consumption/AC_2023.csv`).then(data => {
         generateSvgDataAlc(data)
     });
 
-    generateSvgDataChld(sampleData);
+    d3.csv('../assign3/DV_CSVs/Child Vaccination/CV_2022.csv').then(function(data){
+        generateSvgDataChld(data)
+    })
 
 }
 
